3b889dbd522ce4e40a8e9441d606e740138c2513
IljaNoskov <ilja.noskovv@gmail.com>
2023-10-12 17:52:17 +0300

    fix task3 and pep8

---
+++ task3/roman_to_dec test.py
@@ -0,0 +1,20 @@
+from roman_to_dec import to_dec
+
+# Проверка первых 1000 римских чисел
+f = open('Rom_table.txt', encoding='utf-8')
+roman_and_dec = []
+k = []
+n = 0
+for line in f:
+    if n % 2 == 0:
+        k.append(line[:-1])
+    if len(k) == 2:
+        roman_and_dec.append(k)
+        k = []
+    n += 1
+f.close()
+
+print(roman_and_dec)
+for pair in roman_and_dec:
+    assert to_dec(pair[1]) == int(pair[0]), 'Числа не сошлись'
+print('Программа выдаёь верный резальтат на первой тысяче чисел')
