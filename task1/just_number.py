97f58d7159c5e33d63feae3b8e41b0b6b83c6e04
IljaNoskov <ilja.noskovv@gmail.com>
2023-10-12 17:26:38 +0300

    make task 1 better + pep8

---
+++ task1/just_number.py
@@ -0,0 +1,25 @@
+def prime_numbers(low, high):
+    # создаём пустой список
+    p_list = []
+    # учитываем только неотрицательные числа
+    if low < 0:
+        low = 0
+    # проверяем каждое число из диапазона на простату
+    for i in range(low, high + 1):
+        if is_prime(i) and i != 1:
+            p_list.append(i)
+    return p_list
+
+
+def is_prime(num):
+    # отдельно проверяем двойку
+    if num == 2:
+        return True
+    # рассчитываем число, до которого есть смысл проверять (корень из числа)
+    n = round(pow(num, 1 / 2))
+    # проверяем на делимость все числа от 2 до n - корня
+    for k in range(2, n + 1):
+        if num % k == 0:
+            return False
+    # если прошли все числа и не встретили делителя, то число простое
+    return True
---
+++ task1/prime numbers test.py
@@ -0,0 +1,17 @@
+from just_number import prime_numbers
+
+# Берём из интернета массив простых чисел от 1 до 100000
+# Сайт-источник https://1cov-edu.ru/calc/tablitsa-prostyh-chisel/?n1=1&n2=100000&separator=%20
+f = open('prime_numbers 1-100000.txt')
+true_prime_numbers = f.read().split()
+for i in range(len(true_prime_numbers)):
+    true_prime_numbers[i] = int(true_prime_numbers[i])
+
+my_prime_numbers = prime_numbers(1, 100000)
+
+# Проверяем на равенство результатов
+# В цикле, чтобы узнать, в каких элементаи не совпадение
+for i in range(max(len(true_prime_numbers), len(my_prime_numbers))):
+    assert my_prime_numbers[i] == true_prime_numbers[
+        i], f'Массивы не равны! Ошибка в элементе с индексом {i}, {my_prime_numbers[i]} != {true_prime_numbers[i]}'
+print('Массивы одинаковы')
